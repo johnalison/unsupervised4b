3bdae8b01e861697f229e81ac34327f2b5824284
Sindhu Murthy <sindhum@andrew.cmu.edu>
2023-01-20 09:53:11 -0600

    Changed arguements for condor submission

--- reweight/calculateDtoMWeights.py
+++ reweight/calculateDtoMWeights.py
@@ -1,54 +1,39 @@
 import argparse
 import numpy as np
 import uproot # https://github.com/scikit-hep/uproot3 is in lcg_99cuda   
-from kdtree import getDtoM, getDtoM_chunk                                                                                                                                                                         
+from kdtree import getDtoM_chunk, getDtoM                                                                                                                                                                     
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--data', default="root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed2017_3bDvTMix4bDvT_v0/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef.root")
-parser.add_argument('-t', '--ttbar', default="root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/TTTo2L2Nu2017_4b_noPSData_wTrigW/picoAOD_4b_wJCM_newSBDef.root")
-parser.add_argument('-pY', '--processYr', default="TTTo2L2Nu2017")
+# parser.add_argument('-d', '--data', default="root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed2017_3bDvTMix4bDvT_v0/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef.root")
+# parser.add_argument('-t', '--ttbar', default="root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/TTTo2L2Nu2017_4b_noPSData_wTrigW/picoAOD_4b_wJCM_newSBDef.root")
+# parser.add_argument('-o', '--outputfile', default="picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_wkdt.root")
+parser.add_argument('-m', '--mixed', default="1")
+parser.add_argument('-s', '--selfCount', default="0")
+parser.add_argument('-dYr', '--dataYear', default="2017")
+parser.add_argument('-ttYr', '--ttYear', default="2017")
+parser.add_argument('-ttP', '--ttProcess', default="TTTo2L2Nu")
 parser.add_argument('-v', '--version', default="0")
-parser.add_argument('-bf', '--binEdgeFile', default="../m4jBinEdges.txt")
-parser.add_argument('-bin', '--bin', default=1)
-
-parser.add_argument('-o', '--outputfile', default="picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_wDtoM.root")
-
-#parser.add_argument('--kdTreeData', default="root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/XXX")
-#parser.add_argument('--kdTreesTTbar', default="[root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/XXX]")
-parser.add_argument('--debug', action="store_true", help='')
+parser.add_argument('-bf', '--binEdgeFile', default="unsupervised4b/m4jBinEdges.txt")#"../m4jBinEdges.txt")
+# parser.add_argument('--debug', action="store_true", help='')
 args = parser.parse_args()
 
 
-print("Reading in data:\n",args.data)
-print("Reading in ttbar:\n",args.ttbar)
+# print("Reading in data:\n",args.data)
+# print("Reading in ttbar:\n",args.ttbar)
 
-dataFile   = uproot.open(args.data)['Events']
-# ttbarFile = uproot.open(args.ttbar)['Events']
+if args.mixed == "1":
+    outputfile = "picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+args.version+"_newSBDef_"+args.ttProcess+args.ttYear+"_wkdt.root"
+    TTFilename = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/"+args.ttProcess+args.ttYear+"_4b_noPSData_wTrigW/picoAOD_4b_wJCM_newSBDef.root"
+    dataFilename = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed"+args.dataYear+"_3bDvTMix4bDvT_v"+args.version+"/picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+args.version+"_newSBDef.root"
+else:
+    outputfile = "picoAOD_3b_wJCM_v"+args.version+"_newSBDef_"+args.ttProcess+args.ttYear+"_wkdt.root" 
+    TTFilename = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/"+args.ttProcess+args.ttYear+"_3b_wTrigW/picoAOD_3b_wJCM_newSBDef.root"
+    dataFilename = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/data"+args.dataYear+"_3b/picoAOD_3b_wJCM_newSBDef.root"
+
+m4jBinEdges = np.array([[98,353],[353,406],[406,444],[444,480],[480,516],[516,558],[558,608],[608,679],[679,796],[796,1199]])
 m4jBinEdges = np.loadtxt(args.binEdgeFile)
-
-# print(ttbarFile.keys())
-
-
-#
-#  Reading in the branches
-#
-
+print(m4jBinEdges)
 mcPsedoTagWeight = "mcPseudoTagWeight_3bDvTMix4bDvT_v" + args.version
-
-arrayNames = [mcPsedoTagWeight,"nSelJets","event","run", "leadStM","sublStM","m4j","passHLT"]
-arrayNames = [mcPsedoTagWeight,"passHLT","leadStM","sublStM","m4j"]
-data       = dataFile.arrays(arrayNames)
-print(arrayNames)
-# data = {key.decode('utf-8'): value for (key, value) in data.items()}
-
-# arrayNamesTTbar = [mcPsedoTagWeight,"nSelJets","event","run", "leadStM","sublStM","m4j","trigWeight_Data","passHLT"]
-# arrayNamesTTbar = [mcPsedoTagWeight,"leadStM","sublStM","m4j","passHLT"]
-# print(arrayNamesTTbar)
-# ttbar      = ttbarFile.arrays(arrayNamesTTbar)
-
-# ttbar = {key.decode('utf-8'): value for (key, value) in ttbar.items()}
-
-print("Branches extracted")
 
 def dataSel(data, binEdge):
     sel = np.asarray(np.where((data['m4j']>binEdge[0]) & (data['m4j']<=binEdge[1]), True,False))
@@ -57,25 +42,48 @@ def dataSel(data, binEdge):
     sel &= np.asarray(np.where(data['passHLT'], True, False))
     return sel
 
-wDtoM = np.zeros(len(data['m4j']))
+
+#
+#  Reading in the branches
+#
+
+dataFile   = uproot.open(dataFilename)['Events']
+arrayNames = [mcPsedoTagWeight,"nSelJets","event","run", "leadStM","sublStM","m4j","passHLT"]
+arrayNames = [mcPsedoTagWeight,"passHLT","leadStM","sublStM","m4j"]
+data       = dataFile.arrays(arrayNames)
+print(arrayNames)
+
+if args.selfCount == "0":
+    ttbarFile = uproot.open(TTFilename)['Events']
+    arrayNamesTTbar = [mcPsedoTagWeight,"nSelJets","event","run", "leadStM","sublStM","m4j","trigWeight_Data","passHLT"]
+    arrayNamesTTbar = [mcPsedoTagWeight,"leadStM","sublStM","m4j","passHLT"]
+    print(arrayNamesTTbar)
+    ttbar      = ttbarFile.arrays(arrayNamesTTbar)
+else:
+    outputfile = (dataFilename.split("/")[-1]).split(".root")[0]+"_"+args.dataYear+"_wkdt.root"
+
+print("Branches extracted")
+
+wkdt = np.zeros(len(data['m4j']))
 for binNo, binEdge in enumerate(m4jBinEdges):
     m4jPass = dataSel(data, binEdge)
-    # m4jPassTT = dataSel(ttbar, binEdge)
     leadStM_data = data['leadStM'][m4jPass]
     sublStM_data = data['sublStM'][m4jPass]
     mcPsdoTagW = data[mcPsedoTagWeight][m4jPass]
-    # leadStM_ttbar = ttbar['leadStM'][m4jPassTT]
-    # sublStM_ttbar = ttbar['sublStM'][m4jPassTT]
-    # mcPsdoTagW_ttbar = ttbar[mcPsedoTagWeight][m4jPassTT]
-    # print("Bin selections obtained for bin", binNo)
 
-    wDtoM[m4jPass] = getDtoM_chunk(leadStM_data, sublStM_data, leadStM_data, sublStM_data, radius = 5, ttWeight = mcPsdoTagW)
-    # wDtoM[m4jPass] = getDtoM_chunk(leadStM_data, sublStM_data, leadStM_ttbar, sublStM_ttbar, radius = 5, ttWeight = mcPsdoTagW_ttbar)
+    if args.selfCount == "0":
+        m4jPassTT = dataSel(ttbar, binEdge)
+        leadStM_ttbar = ttbar['leadStM'][m4jPassTT]
+        sublStM_ttbar = ttbar['sublStM'][m4jPassTT]
+        mcPsdoTagW_ttbar = ttbar[mcPsedoTagWeight][m4jPassTT]
+        wkdt[m4jPass] = getDtoM_chunk(leadStM_data, sublStM_data, leadStM_ttbar, sublStM_ttbar, radius = 5, ttWeight = mcPsdoTagW_ttbar)
+    else:
+        wkdt[m4jPass] = getDtoM_chunk(leadStM_data, sublStM_data, leadStM_data, sublStM_data, radius = 5, ttWeight = mcPsdoTagW)
+    print("Bin selections obtained for bin", binNo)
 
 print("DtoM reweighting complete")
-with uproot.recreate(args.outputfile) as outfile:
-    outfile["Events"] = {"wDtoM": np.array(wDtoM)}
+with uproot.recreate(outputfile) as outfile:
+    outfile["Events"] = {"wkdt": np.array(wkdt)}
     outfile["Events"].show()
 
-
-print(args.outputfile, 'outputfile saved\n')
+print(outputfile, 'outputfile saved\n')
--- reweight/getMultijet.py
+++ reweight/getMultijet.py
@@ -1,108 +1,178 @@
 import numpy as np
 import uproot
 
-yearArr = ['2016', '2017', '2018']
-
+EOSfilepath = "root://cmsxrootd.fnal.gov//store/user/smurthy/condor/unsupervised4b/wkdt/"
+EOSwDtoMpath = "root://cmsxrootd.fnal.gov//store/user/smurthy/condor/unsupervised4b/wDtoM/"
+EOSwDtoMpath = ""
 processArr = ['TTTo2L2Nu', 'TTToHadronic', 'TTToSemiLeptonic']
-#### mixed ####
-for vn in range(0,15):
-    for year in ['2017', '2018']:
-        outputfile = 'multijet/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
-        dataFile = 'wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_wDtoM.root'
-        mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed"+year+"_3bDvTMix4bDvT_v"+str(vn)+"/picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+str(vn)+"_newSBDef.root"
-        data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
-        mcp = np.array(uproot.open(mcpFile)['Events'].arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
-        ttbar = 0
-        for process in processArr:
-            ttFile = 'wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+'_wDtoM.root'
-            ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
-        with uproot.recreate(outputfile) as outfile:
-            outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
-            # outfile["Events"].show()
-        print("Done", year, vn)
+ttYearArr = [['2016_preVFP','2016_postVFP'],['2017'],['2018']]
+dataYearArr = ['2016', '2017', '2018']
+vn = 0
 
-TTprocess = ['_preVFP','_postVFP']
-for vn in range(15):
-    for year in ['2016']:
-        outputfile = 'multijet/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
-        dataFile = 'wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_wDtoM.root'
-        mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed"+year+"_3bDvTMix4bDvT_v"+str(vn)+"/picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+str(vn)+"_newSBDef.root"
-        mcp = np.array(uproot.open(mcpFile)['Events'].arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
-        data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
-        ttbar = 0
-        for process in processArr:
-            for tt in TTprocess:
-                ttFile = 'wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+tt+'_wDtoM.root'
-                ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
-        with uproot.recreate(outputfile) as outfile:
-            outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
-            # outfile["Events"].show()
-        print("Done", year, vn)
+for dYr, ttYr in zip(dataYearArr, ttYearArr):
+    dataFile = "picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+str(vn)+"_newSBDef"+"_"+dYr+"_wkdt.root"
+    outputfile = EOSwDtoMpath + dataFile.split("wkdt.root")[0]+"wDtoM.root"
+    print(outputfile)
+    dataFile = EOSfilepath + dataFile
+    data = np.array(uproot.open(dataFile)['Events'].arrays('wkdt'))['wkdt']
+    ttbar = 0
+    for process in processArr:
+        for ttYrPr in ttYr:
+            ttFile = EOSfilepath+'picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+process+ttYrPr+'_wkdt.root'
+            ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wkdt'))['wkdt']
+    print(dYr, len(ttbar), len(data))
+    with uproot.recreate(outputfile) as outfile:
+        outfile["Events"] = {"wDtoM": np.divide(data-ttbar,data, where=data!=0)}
+        outfile["Events"].show()
 
-
-#### 3b Data #####
-import numpy as np
-import uproot
-
-yearArr = ['2016', '2017', '2018']
-
-processArr = ['TTTo2L2Nu', 'TTToHadronic', 'TTToSemiLeptonic']
-
-for year in ['2017', '2018']:
-    mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/data"+year+"_3b/picoAOD_3b_wJCM_newSBDef.root"
-    mcpBranch = uproot.open(mcpFile)['Events']
-    for vn in range(0,15):
-        outputfile = 'multijet/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
-        dataFile = "wDtoMSelf/picoAOD_3b_wJCM_v"+str(vn)+"_newSBDef_"+year+"_wDtoM.root"   
-        data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
-        mcp = np.array(mcpBranch.arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
-        ttbar = 0
-        for process in processArr:
-            ttFile = 'wDtoM3b/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+'_wDtoM.root'
-            ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
-        with uproot.recreate(outputfile) as outfile:
-            outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
-            # outfile["Events"].show()
-        print("Done", year, vn)
-
-TTprocess = ['_preVFP','_postVFP']  
-for year in ['2016']:
-    mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/data"+year+"_3b/picoAOD_3b_wJCM_newSBDef.root"
-    mcpBranch = uproot.open(mcpFile)['Events']
-    for vn in range(15):
-        outputfile = 'multijet/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
-        dataFile = "wDtoMSelf/picoAOD_3b_wJCM_v"+str(vn)+"_newSBDef_"+year+"_wDtoM.root"
-        data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
-        mcp = np.array(mcpBranch.arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
-        ttbar = 0
-        for process in processArr:
-            for tt in TTprocess:
-                ttFile = 'wDtoM3b/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+tt+'_wDtoM.root'
-                ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
-        with uproot.recreate(outputfile) as outfile:
-            outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
-            # outfile["Events"].show()
-        print("Done", year, vn)
+for dYr, ttYr in zip(dataYearArr, ttYearArr):
+    dataFile = "picoAOD_3b_wJCM_newSBDef"+"_"+dYr+"_wkdt.root"
+    outputfile = EOSwDtoMpath + dataFile.split("wkdt.root")[0]+"wDtoM.root"
+    dataFile = EOSfilepath + dataFile
+    data = np.array(uproot.open(dataFile)['Events'].arrays('wkdt'))['wkdt']
+    ttbar = 0
+    for process in processArr:
+        for ttYrPr in ttYr:
+            ttFile = EOSfilepath+'picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+ttYrPr+'_wkdt.root'
+            ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wkdt'))['wkdt']
+    print(dYr, len(ttbar), len(data))
+    with uproot.recreate(outputfile) as outfile:
+        outfile["Events"] = {"wDtoM": np.divide(data-ttbar,data, where=data!=0)}
+        # outfile["Events"].show()
+    
 
 
-### For Data ###
-'''
-wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2016_wDtoM.root
-wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2017_wDtoM.root
-wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2018_wDtoM.root
+# for dYr, ttYr in zip(dataYearArr, ttYearArr):
+#     outputfile = 'picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+dYr+'_wDtoM.root'
+#     dataFile = 'picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+dYr+'_wkdt.root'
+#     data = np.array(uproot.open(dataFile)['Events'].arrays('wkdt'))['wkdt']
+#     ttbar = 0
+#     for process in processArr:
+#         for ttYrPr in ttYr:
+#             ttFile = EOSfilepath+'picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+ttYr+'_wkdt.root'
+#             ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wkdt'))['wkdt']
 
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2016_postVFP_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2016_preVFP_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2016_postVFP_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2016_preVFP_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2016_postVFP_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2016_preVFP_wDtoM.root
+#         with uproot.recreate(outputfile) as outfile:
+#                 outfile["Events"] = {"wDtoM": np.divide(data-ttbar,data)}
+#                 # outfile["Events"].show()
 
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2017_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2017_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2017_wDtoM.root
 
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2018_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2018_wDtoM.root
-wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2018_wDtoM.root
-'''
+# for ttYr in ttYearArr:
+#     dataFile = 'picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_wDtoM.root'
+#     for p in processArr:
+
+# outputfile = "picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+args.version+"_newSBDef_"+args.ttProcess+args.ttYear+"_wkdt.root"
+
+
+# "picoAOD_3b_wJCM_v"+args.version+"_newSBDef_"+args.ttProcess+args.ttYear+"_wkdt.root" 
+
+
+
+# import numpy as np
+# import uproot
+
+# yearArr = ['2016', '2017', '2018']
+
+# processArr = ['TTTo2L2Nu', 'TTToHadronic', 'TTToSemiLeptonic']
+# #### mixed ####
+# for vn in range(0,15):
+#     for year in ['2017', '2018']:
+#         outputfile = 'multijet/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
+#         dataFile = 'wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_wDtoM.root'
+#         mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed"+year+"_3bDvTMix4bDvT_v"+str(vn)+"/picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+str(vn)+"_newSBDef.root"
+#         data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         mcp = np.array(uproot.open(mcpFile)['Events'].arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
+#         ttbar = 0
+#         for process in processArr:
+#             ttFile = 'wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+'_wDtoM.root'
+#             ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         with uproot.recreate(outputfile) as outfile:
+#             outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
+#             # outfile["Events"].show()
+#         print("Done", year, vn)
+
+# TTprocess = ['_preVFP','_postVFP']
+# for vn in range(15):
+#     for year in ['2016']:
+#         outputfile = 'multijet/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
+#         dataFile = 'wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_wDtoM.root'
+#         mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/mixed"+year+"_3bDvTMix4bDvT_v"+str(vn)+"/picoAOD_3bDvTMix4bDvT_4b_wJCM_v"+str(vn)+"_newSBDef.root"
+#         mcp = np.array(uproot.open(mcpFile)['Events'].arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
+#         data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         ttbar = 0
+#         for process in processArr:
+#             for tt in TTprocess:
+#                 ttFile = 'wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+tt+'_wDtoM.root'
+#                 ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         with uproot.recreate(outputfile) as outfile:
+#             outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
+#             # outfile["Events"].show()
+#         print("Done", year, vn)
+
+
+# #### 3b Data #####
+# import numpy as np
+# import uproot
+
+# yearArr = ['2016', '2017', '2018']
+
+# processArr = ['TTTo2L2Nu', 'TTToHadronic', 'TTToSemiLeptonic']
+
+# for year in ['2017', '2018']:
+#     mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/data"+year+"_3b/picoAOD_3b_wJCM_newSBDef.root"
+#     mcpBranch = uproot.open(mcpFile)['Events']
+#     for vn in range(0,15):
+#         outputfile = 'multijet/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
+#         dataFile = "wDtoMSelf/picoAOD_3b_wJCM_v"+str(vn)+"_newSBDef_"+year+"_wDtoM.root"   
+#         data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         mcp = np.array(mcpBranch.arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
+#         ttbar = 0
+#         for process in processArr:
+#             ttFile = 'wDtoM3b/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+'_wDtoM.root'
+#             ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         with uproot.recreate(outputfile) as outfile:
+#             outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
+#             # outfile["Events"].show()
+#         print("Done", year, vn)
+
+# TTprocess = ['_preVFP','_postVFP']  
+# for year in ['2016']:
+#     mcpFile = "root://cmsxrootd.fnal.gov//store/user/jda102/condor/ZH4b/ULTrig/data"+year+"_3b/picoAOD_3b_wJCM_newSBDef.root"
+#     mcpBranch = uproot.open(mcpFile)['Events']
+#     for vn in range(15):
+#         outputfile = 'multijet/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+year+'_multijet.root'
+#         dataFile = "wDtoMSelf/picoAOD_3b_wJCM_v"+str(vn)+"_newSBDef_"+year+"_wDtoM.root"
+#         data = np.array(uproot.open(dataFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         mcp = np.array(mcpBranch.arrays("mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)))["mcPseudoTagWeight_3bDvTMix4bDvT_v"+str(vn)]
+#         ttbar = 0
+#         for process in processArr:
+#             for tt in TTprocess:
+#                 ttFile = 'wDtoM3b/picoAOD_3b_wJCM_v'+str(vn)+'_newSBDef_'+process+year+tt+'_wDtoM.root'
+#                 ttbar += np.array(uproot.open(ttFile)['Events'].arrays('wDtoM'))['wDtoM']
+#         with uproot.recreate(outputfile) as outfile:
+#             outfile["Events"] = {"wDtoM": np.divide(np.multiply(mcp,(data-ttbar)),data)}
+#             # outfile["Events"].show()
+#         print("Done", year, vn)
+
+
+# ### For Data ###
+# '''
+# wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2016_wDtoM.root
+# wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2017_wDtoM.root
+# wDtoMSelf/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_2018_wDtoM.root
+
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2016_postVFP_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2016_preVFP_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2016_postVFP_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2016_preVFP_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2016_postVFP_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2016_preVFP_wDtoM.root
+
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2017_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2017_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2017_wDtoM.root
+
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTTo2L2Nu2018_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToHadronic2018_wDtoM.root
+# wDtoM/picoAOD_3bDvTMix4bDvT_4b_wJCM_v0_newSBDef_TTToSemiLeptonic2018_wDtoM.root
+# '''
